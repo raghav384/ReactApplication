88aaa6e8b604bb3c5952b81342f3462ccafbc5bf
raghav384 <bansalraghav384@gmail.com>
2021-05-02 16:33:33 +0530

    Redirection Blog Insertion
    Redirection and Blog Insertion Components

--- frontend/src/components/Redirect/redirect.jsx
+++ frontend/src/components/Redirect/redirect.jsx
@@ -1,40 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Lottie from "react-lottie";
 import Animation from '../../animations/9513-preloader.json'
-import {useHistory, useLocation} from 'react-router-dom'
 import './redirect.css';
 
-export default () => {
-    const history=useHistory();
-    const location=useLocation();
+class Redirect extends Component {
+    defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: Animation,
 
-    const defaultOptions = {
-      loop: true,
-      autoplay: true,
-      animationData: Animation,
+    rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+    
 
-      rendererSettings: {
-        preserveAspectRatio: "xMidYMid slice",
-       
-    },
-    };
-    React.useEffect(()=>{
+},
+};
+   
+componentDidMount() {
+    const { history } = this.props;
+    console.log(this.props.location.sampleParam);
 
-        if(location&&location.state&&location.state.redirectUrl)
-        setTimeout(() => {
-                window.location.replace(location&&location.state&&location.state.redirectUrl?location.state.redirectUrl:"")
-            }, location&&location.state&&location.state.redirectTime?location.state.redirectTime:0);
-    },[])
-    return  <div className="con-flex">
+setTimeout(() => {
+    window.location.replace(this.props.location.redirectUrl)
+    }, this.props.location.redirectTime);
+    }
+
+render() {
+    return (
+        <div className="con-flex">
+
         <div className="d-flex-main d-col">
-            <div className="d-flex-main d-row">
-          <img src={location&&location.state&&location.state.fromimg?location.state.fromimg:undefined} className="w-img"/>
-            <div className="w-img">
-            <Lottie options={defaultOptions} />
-            </div>
-            <img src={location&&location.state&&location.state.toimg?location.state.toimg:undefined} className="w-img"/>
-            </div>
-        <h2 className="f-head">You found a great deal on <span>{location&&location.state&&location.state.from?location.state.from:undefined}</span> we will be redirecting to the following websites <span>{location&&location.state&&location.state.to?location.state.to:undefined}</span>  </h2>
+        <div className="d-flex-main d-row">
+        <img src="http://localhost:8000/api/image_retriever/HealthScroll.png" className="w-img"/>
+        <div className="w-img">
+        <Lottie options={this.defaultOptions}/>
         </div>
-    </div>
-}
+        <img src={this.props.location.destination_image_href} className="w-img"/>
+        </div>
+        <h2 className="f-head">You found a great deal on <span>HealthScroll</span> we will be redirecting to the following
+        website <span>{this.props.location.destination_name}</span></h2>
+        </div>
+        </div>
+        );
+        };
+        }
+
+export default Redirect;
